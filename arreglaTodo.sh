4a5d32a2d20b1cec8da0d5e4d2681965283d84a9
extensionTec <octavio.martinez@itesm.mx>
2013-08-26 08:23:43 -0500

    Otra vez arreglado

--- arreglaTodo.sh
+++ arreglaTodo.sh
@@ -1,6 +1,7 @@
 perl -pi -e 's/<title>envioRegular.html<\/title>//gi' *.html
 perl -pi -e 's/<a href="#">aviso de/<a href="http:\/\/www.itesm.edu\/wps\/wcm\/connect\/ITESM\/Tecnologico+de+Monterrey\/Herramientas\/Aviso+de+privacidad\/">aviso de/gi' *.html
-perl -pi -e 's/<td colspan="2" height="10"><\/td>/<td colspan="2"><img src="http:\/\/sadpanda.us\/images\/1810109-ZB1Z86U.png"><\/td>/g' *.html
+perl -pi -e 's/<td colspan="2" height="10"><\/td>/<td colspan="2"><img class="tamanoMaximo" src="http:\/\/sadpanda.us\/images\/1810109-ZB1Z86U.png"><\/td>/g' *.html
+perl -pi -e 's/<td height="10"><\/td>/<td colspan="2"><img class="tamanoMaximo" src="http:\/\/sadpanda.us\/images\/1810109-ZB1Z86U.png"><\/td>/g' *.html
 perl -pi -e 's/type="text\/css">/type="text\/css">\n.bg {background-color:#0E1F31;}/gi' *.html 
 
 for file in *.html
@@ -8,4 +9,4 @@ do
 	mv $file ${file}.old
 	tidy -i -o $file ${file}.old
 done
-#rm -f *.html.old
+rm -f *.html.old

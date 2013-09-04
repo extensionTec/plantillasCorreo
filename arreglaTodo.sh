6445a6d640b8e9eb373263e51a607b6964dfd5dc
extensionTec <octavio.martinez@itesm.mx>
2013-09-04 08:26:44 -0500

    Añadí nota para los correos de confirmación

--- arreglaTodo.sh
+++ arreglaTodo.sh
@@ -1,4 +1,4 @@
 perl -pi -e 's/%5B/\[/gi' *Inline.html
 perl -pi -e 's/%5D/\]/gi' *Inline.html
-perl -pi -e 's/<meta name="generator" content=//gi' *.html
-perl -pi -e 's/"HTML Tidy for HTML5 \(experimental\) for Linux\/x86 https:\/\/github.com\/w3c\/tidy-html5\/tree\/c63cc39">//gi' *.html
+#perl -pi -e 's/<meta name="generator" content=//gi' *.html
+#perl -pi -e 's/"HTML Tidy for HTML5 \(experimental\) for Linux\/x86 https:\/\/github.com\/w3c\/tidy-html5\/tree\/c63cc39">//gi' *.html
